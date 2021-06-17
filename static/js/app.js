bb4c3a2b00c344758bdbf5ca36016042e7ddbdfc
Guadalupe Gomez <g.gop112019@gmail.com>
2021-06-17 11:06:24 -0500

    Si se pudo perros💥

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,29 @@
 // https://rickandmortyapi.com/api/character/2
 import API from "./api.js";
+import Character from "./character.js";
 const api = new API();
+let currenCharacter = 4;
 
-const $characterContainer = document.querySelector("#character-container");
+const $loadNext = document.querySelector("#load-next");
 
-// const characterTemplate = `
-// <article class="character">
-// <div class="character-grid">
-//   <h2>Rick Sanchez</h2>
-//   <img src="" alt="" />
-// </div>
-// </article>
-// `;
-
-class Character {
-  constructor({ name, image }) {
-    this.name = name;
-    this.image = image;
-    this.render();
-  }
-  build() {
-    return `
-      <article class="character">
-    <div class="character-grid">
-    <h2>${this.name}</h2>
-    <img src=${this.image} alt="" />
-    </div>
-    </article>
-      `;
-  }
-  render() {
-    $characterContainer.innerHTML = this.build();
-  }
-}
+$loadNext.addEventListener("click", async () => {
+  const characterData = await api.getCharacter(++currenCharacter);
+  console.log(characterData);
+  new Character(characterData);
+});
 
 // arracamos la app
 async function initApp(initCharacter) {
   const characterData = await api.getCharacter(initCharacter);
   console.log(characterData);
-  //   const rick = new Character(characterData);
+  new Character(characterData);
+
   //   const rick = new Character({
   //     name: "pancho",
   //     img: "http://127.0.0.1:5500/static/images/rickandmorty.png",
   //   });
 }
-initApp(1);
+initApp(currenCharacter);
 
 // rick.render();
 // console.log(api.getCharacter(3));
